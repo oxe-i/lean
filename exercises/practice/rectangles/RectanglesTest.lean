c29f9a7e80e4d246c8116a4f8756d7e1401bf7c1
keiravillekode <keiravillekode@gmail.com>
2026-02-07 09:46:57 +1100

    rectangles: generate using serializeList (#104)
    [no important files changed]

--- exercises/practice/rectangles/RectanglesTest.lean
+++ exercises/practice/rectangles/RectanglesTest.lean
@@ -8,31 +8,96 @@ def rectanglesTests : TestSuite :=
   |>.addTest "no rows" (do
       return assertEqual 0 (Rectangles.rectangles #[]))
   |>.addTest "no columns" (do
-      return assertEqual 0 (Rectangles.rectangles #[""]))
+      return assertEqual 0 (Rectangles.rectangles #[
+        ""
+      ]))
   |>.addTest "no rectangles" (do
-      return assertEqual 0 (Rectangles.rectangles #[" "]))
+      return assertEqual 0 (Rectangles.rectangles #[
+        " "
+      ]))
   |>.addTest "one rectangle" (do
-      return assertEqual 1 (Rectangles.rectangles #["+-+","| |","+-+"]))
+      return assertEqual 1 (Rectangles.rectangles #[
+        "+-+",
+        "| |",
+        "+-+"
+      ]))
   |>.addTest "two rectangles without shared parts" (do
-      return assertEqual 2 (Rectangles.rectangles #["  +-+","  | |","+-+-+","| |  ","+-+  "]))
+      return assertEqual 2 (Rectangles.rectangles #[
+        "  +-+",
+        "  | |",
+        "+-+-+",
+        "| |  ",
+        "+-+  "
+      ]))
   |>.addTest "five rectangles with shared parts" (do
-      return assertEqual 5 (Rectangles.rectangles #["  +-+","  | |","+-+-+","| | |","+-+-+"]))
+      return assertEqual 5 (Rectangles.rectangles #[
+        "  +-+",
+        "  | |",
+        "+-+-+",
+        "| | |",
+        "+-+-+"
+      ]))
   |>.addTest "rectangle of height 1 is counted" (do
-      return assertEqual 1 (Rectangles.rectangles #["+--+","+--+"]))
+      return assertEqual 1 (Rectangles.rectangles #[
+        "+--+",
+        "+--+"
+      ]))
   |>.addTest "rectangle of width 1 is counted" (do
-      return assertEqual 1 (Rectangles.rectangles #["++","||","++"]))
+      return assertEqual 1 (Rectangles.rectangles #[
+        "++",
+        "||",
+        "++"
+      ]))
   |>.addTest "1x1 square is counted" (do
-      return assertEqual 1 (Rectangles.rectangles #["++","++"]))
+      return assertEqual 1 (Rectangles.rectangles #[
+        "++",
+        "++"
+      ]))
   |>.addTest "only complete rectangles are counted" (do
-      return assertEqual 1 (Rectangles.rectangles #["  +-+","    |","+-+-+","| | -","+-+-+"]))
+      return assertEqual 1 (Rectangles.rectangles #[
+        "  +-+",
+        "    |",
+        "+-+-+",
+        "| | -",
+        "+-+-+"
+      ]))
   |>.addTest "rectangles can be of different sizes" (do
-      return assertEqual 3 (Rectangles.rectangles #["+------+----+","|      |    |","+---+--+    |","|   |       |","+---+-------+"]))
+      return assertEqual 3 (Rectangles.rectangles #[
+        "+------+----+",
+        "|      |    |",
+        "+---+--+    |",
+        "|   |       |",
+        "+---+-------+"
+      ]))
   |>.addTest "corner is required for a rectangle to be complete" (do
-      return assertEqual 2 (Rectangles.rectangles #["+------+----+","|      |    |","+------+    |","|   |       |","+---+-------+"]))
+      return assertEqual 2 (Rectangles.rectangles #[
+        "+------+----+",
+        "|      |    |",
+        "+------+    |",
+        "|   |       |",
+        "+---+-------+"
+      ]))
   |>.addTest "large input with many rectangles" (do
-      return assertEqual 60 (Rectangles.rectangles #["+---+--+----+","|   +--+----+","+---+--+    |","|   +--+----+","+---+--+--+-+","+---+--+--+-+","+------+  | |","          +-+"]))
+      return assertEqual 60 (Rectangles.rectangles #[
+        "+---+--+----+",
+        "|   +--+----+",
+        "+---+--+    |",
+        "|   +--+----+",
+        "+---+--+--+-+",
+        "+---+--+--+-+",
+        "+------+  | |",
+        "          +-+"
+      ]))
   |>.addTest "rectangles must have four sides" (do
-      return assertEqual 5 (Rectangles.rectangles #["+-+ +-+","| | | |","+-+-+-+","  | |  ","+-+-+-+","| | | |","+-+ +-+"]))
+      return assertEqual 5 (Rectangles.rectangles #[
+        "+-+ +-+",
+        "| | | |",
+        "+-+-+-+",
+        "  | |  ",
+        "+-+-+-+",
+        "| | | |",
+        "+-+ +-+"
+      ]))
 
 def main : IO UInt32 := do
   runTestSuitesWithExitCode [rectanglesTests]
--- generators/Generator/Generator/RectanglesGenerator.lean
+++ generators/Generator/Generator/RectanglesGenerator.lean
@@ -17,12 +17,13 @@ def {exercise.decapitalize}Tests : TestSuite :=
   (TestSuite.empty \"{exercise}\")"
 
 def genTestCase (exercise : String) (case : TreeMap.Raw String Json) : String :=
-  let input := case.get! "input"
+  let strings := case.get! "input"
+              |> (·.getObjValD "strings")
   let expected := case.get! "expected"
   let description := case.get! "description"
               |> (·.compress)
   let funName := getFunName (case.get! "property")
-  let call := s!"({exercise}.{funName} #{insertAllInputs input})"
+  let call := s!"({exercise}.{funName} #{serializeList strings})"
   s!"
   |>.addTest {description} (do
       return assertEqual {expected} {call})"
